d68eaedb7eaae6c443e2519bc0a12203b08483c1
JasperMunene <jaspermunene2022@gmail.com>
2024-11-27 17:19:04 +0300

    Complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    evens = list()
+    for n in num_list:
+        if n % 2 == 0:
+            evens.append(n)
+    return evens
     pass
 
 def make_exclamation(sentence_list):
+    text = list()
+    for n in sentence_list:
+        n = f"{n}!"
+        text.append(n)
+    return text
     pass
